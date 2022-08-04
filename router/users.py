a004978d2f661a50ec3155410ad72f85e26368a1
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-08-04 12:09:36 -0400

    ocultar detalles innecesarios de la respuesta (modelo de respuesta)

--- router/users.py
+++ router/users.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, APIRouter
 from database import get_db
 from models import User
-from schemas import UserCreate
+from schemas import UserCreate, ShowUser
 from hashing import Hasher
 from sqlalchemy.orm import Session
 
@@ -11,7 +11,7 @@ router = APIRouter()
 def index():
     return  {"Message": "hello word"}
 
-@router.post('/users', tags=["user"])
+@router.post('/users', tags=["user"], response_model=ShowUser)
 def create_user(user: UserCreate, db:Session=Depends(get_db)):
     user = User(email=user.email, password=Hasher.get_hash_password(user.password))
     db.add(user)
