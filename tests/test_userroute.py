79361c9b765ac3c9e9c7c0afbdf624ff9a6f5374
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-08-04 18:32:39 -0400

    Testing the Database (Dependency overrides)

--- tests/test_userroute.py
+++ tests/test_userroute.py
@@ -1,12 +1,38 @@
 from fastapi.testclient import TestClient
 import json
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from main import app
+from database import Base, get_db
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
