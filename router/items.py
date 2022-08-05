299fd4fd9d301877924a2a71cf11ea541a8e46c9
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-08-05 15:22:33 -0400

    updated the Item endpoint(PUT Endpoint)

--- router/items.py
+++ router/items.py
@@ -3,6 +3,7 @@ from fastapi import Depends, APIRouter, HTTPException, status
 from database import get_db
 from sqlalchemy.orm import Session
 from models import Items
+from fastapi.encoders import jsonable_encoder
 from schemas import ItemCreate, ShowItem
 from datetime import datetime
 
@@ -30,3 +31,13 @@ def retrieve_item_by_id(id: int, db:Session=Depends(get_db)):
     if not item:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not found")
     return item
+
+@router.put("/item/update/{id}", tags=["items"])
+def update_item_by_id(id:int, item: ItemCreate, db:Session=Depends(get_db)):
+    existing_item = db.query(Items).filter(Items.id == id)
+    if not existing_item.first():
+        return {"message": f"No details exists for Item ID {id}"}
+    # existing_item.update(jsonable_encoder(item))
+    existing_item.update(item.__dict__)
+    db.commit()
+    return {"message": f"Details for Item ID {id} successfully updated"}
