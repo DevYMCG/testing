6753d782417f7c69c7c071d482d922674d6f1f4b
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-08-08 19:18:59 -0400

    serving templates using Jinja2

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from config import setting
 from database import engine
 from models import Base
 from router import items, users, login
+from webapps.routers import items as web_items
 
 Base.metadata.create_all(bind=engine)
 
@@ -30,3 +31,4 @@ app = FastAPI(
 app.include_router(users.router)
 app.include_router(items.router)
 app.include_router(login.router)
+app.include_router(web_items.router)
