eca417d7e01d85c4de05bad76e616a596eb67939
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-08-10 08:11:06 -0400

    List all the Items in HTML Template

--- webapps/routers/items.py
+++ webapps/routers/items.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
+from models import Items
+from sqlalchemy.orm import Session
+from database import get_db
 
 router = APIRouter()
 
 templates = Jinja2Templates(directory="templates")
 
 @router.get("/")
-def item_home(request: Request):
-    return templates.TemplateResponse("item_homepage.html", {"request": request})
+def item_home(request: Request, db:Session=Depends(get_db)):
+    items = db.query(Items).all()
+    return templates.TemplateResponse("item_homepage.html", {"request": request, "items": items})
