f257e5fcc668d6f79ea776f3527cf0555a0d1b96
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-08-11 11:25:41 -0400

    Implement Login Logic and store JWT Token as HTTP Only

--- webapps/routers/auth.py
+++ webapps/routers/auth.py
@@ -1,6 +1,11 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends, Response
 from fastapi.templating import Jinja2Templates
-
+from sqlalchemy.orm import Session
+from database import get_db
+from models import User
+from hashing import Hasher
+from jose import jwt
+from config import setting
 
 router = APIRouter(include_in_schema=False)
 templates = Jinja2Templates(directory="templates")
@@ -8,3 +13,34 @@ templates = Jinja2Templates(directory="templates")
 @router.get("/login")
 def login(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
+
+
+@router.post("/login")
+async def login(request: Request, response: Response, db: Session=Depends(get_db)):
+    form = await request.form()
+    email = form.get("email")
+    password = form.get("password")
+    errors = []
+    if not email:
+        errors.append("Please enter valid email")
+    if not password or len(password) < 5:
+        errors.append("Password should be > 5 chars")
+    try:
+        user = db.query(User).filter(User.email==email).first()
+        if user is None:
+            errors.append("Email does not exists")
+            return templates.TemplateResponse("login.html", {"request": request, "errors": errors})
+        else:
+            if Hasher.verify_password(password, user.password):
+                data = {"sub": email}
+                jwt_token = jwt.encode(data, setting.SECRET_KEY, algorithm=setting.ALGORITHM)
+                msg = "Login Successfull"
+                response = templates.TemplateResponse("login.html", {"request": request, "msg": msg})
+                response.set_cookie(key="access_token", value=f"Bearer {jwt_token}", httponly=True)
+                return response
+            else:
+                errors.append("Invalid password")
+                return templates.TemplateResponse("login.html", {"request": request, "errors": errors})
+    except:
+        errors.append("Something Wrong it")
+        return templates.TemplateResponse("login.html", {"request": request, "errors": errors})
